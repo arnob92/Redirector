66305183b8d691969e688cccc0c1053e0c8b4b4d
Md. Ahsanul Karim <ahsan.arnob.92@gmail.com>
2025-12-12 06:11:15 +0600

    ggg

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.set("trust proxy", true);
 // -------------------------------
 const PORT = process.env.PORT || 3000;
 const ADMIN_TOKEN = process.env.ADMIN_TOKEN;
-const DISABLE_ADMIN = process.env.DISABLE_ADMIN;
+const DISABLE_ADMIN = process.env.DISABLE_ADMIN === "1";
 
 const CITIES_FILE = path.join(__dirname, "cities.json");
 const IPAPI_TIMEOUT_MS = 4000;
